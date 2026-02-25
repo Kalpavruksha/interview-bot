5f1a9c261168c6062790d516056d6cee574efb16
kalpavruksha G <154514800+Kalpavruksha@users.noreply.github.com>
2026-02-26 00:31:54 +0530

    Update rag_server.py

--- rag_server.py
+++ rag_server.py
@@ -8,7 +8,6 @@ import chromadb
 from chromadb.config import Settings
 from dotenv import load_dotenv
 import google.generativeai as genai
-
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
